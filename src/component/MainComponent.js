d36c14a698c27fe59f005edb31687d043564177c
junaet faruk rafi <rafee790@gmail.com>
2022-08-20 13:39:34 +0600

    Component Tree

---
+++ src/component/MainComponent.js
@@ -0,0 +1,17 @@
+import React from 'react'
+import Heder from './header/Heder';
+import Body from './body/Body';
+import Footer from './footer/Footer';
+
+
+const MainComponent = () => {
+  return (
+    <div>
+        <Heder/>
+        <Body/>
+        <Footer/>
+    </div>
+  )
+}
+
+export default MainComponent;
