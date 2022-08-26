b2477d819307b92993d764ec9dfe6d62a6fab06c
junaet faruk rafi <rafee790@gmail.com>
2022-08-26 14:38:03 +0600

    test

--- src/component/body/Home.js
+++ src/component/body/Home.js
@@ -3,7 +3,7 @@ import React, { Component } from "react";
 class Home extends Component {
   render() {
     document.title = "Restaurant";
-    return <div>ruhruhru</div>;
+    return <div>ruhruhru dfdfd</div>;
   }
 }
 
